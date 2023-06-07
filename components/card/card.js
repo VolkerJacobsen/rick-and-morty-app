690f5fff132b4a59b4e0f5120af40fd77eb15a77
Volker Jacobsen <volkerjacobsen@gmail.com>
2023-06-07 10:40:07 +0200

    deleted querySelector

--- components/card/card.js
+++ components/card/card.js
@@ -1,5 +1,4 @@
 export function createCharacterCard(name, status, type, occurrences, src) {
-  const cardContainer = document.querySelector('["card-container"]');
   const newCard = document.createElement("li");
   newCard.classList.add("card");
   newCard.innerHTML = `<div class="card__image-container">
